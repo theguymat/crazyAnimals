893c66c1732f3fefdd3ebdf706f061a21787b5fa
6454 <6454@ait-sydney.net.au>
2018-08-30 16:49:42 +1000

    added positions

--- MouseHandler.cpp
+++ MouseHandler.cpp
@@ -12,18 +12,18 @@ MouseHandler::~MouseHandler()
 
 void MouseHandler::update(SDL_Event* event){
 
-	if (event->motion.x >= /*object1->pos.x*/ && event->motion.x <= /*object1->pos.x + 60*/ && event->motion.y >= /*object1->pos.y*/ && event->motion.y <= /*object1->pos.y + 32*/){
+	if (event->motion.x >= 100 && event->motion.x <= 250 && event->motion.y >= 100 && event->motion.y <= 160){
 		Menu->button1 = true;//change button state to change its colour
 	}
-	else if (event->motion.x >= /*object2->pos.x*/ && event->motion.x <= /*object2->pos.x + 60*/ && event->motion.y >= /*object2->pos.y*/ && event->motion.y <= /*object2->pos.y + 32*/){
+	else if (event->motion.x >= 100 && event->motion.x <= 430 && event->motion.y >= 200 && event->motion.y <= 260){
 		Menu->button2 = true;
 	}
-	else if (event->motion.x >= /*object3->pos.x*/ && event->motion.x <= /*object3->pos.x + 60*/ && event->motion.y >= /*object3->pos.y*/ && event->motion.y <= /*object3->pos.y + 32*/){
+	else if (event->motion.x >= 100 && event->motion.x <= 240 && event->motion.y >= 300 && event->motion.y <= 360){
 		Menu->button3 = true;
 	}
 	else
 	{
-		Menu->button1 = false;//reset all button states when not over button
+		Menu->button1 = false;
 		Menu->button2 = false;
 		Menu->button3 = false;
 	}
